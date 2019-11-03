fe08a059f51fdac586cb672a0a72fb264447de07
Green Luo <greenlaw110@gmail.com>
2019-11-03 12:46:16 +1100

    add login page

--- src/main/java/demo/chatroom/ChatApp.java
+++ src/main/java/demo/chatroom/ChatApp.java
@@ -21,23 +21,45 @@ package demo.chatroom;
  */
 
 import act.Act;
+import act.app.ActionContext;
 import act.ws.WebSocketContext;
+import org.osgl.mvc.annotation.Before;
 import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.annotation.PostAction;
 import org.osgl.mvc.annotation.WsAction;
 import org.osgl.util.S;
+
+import static act.controller.Controller.Util.redirect;
 
 @SuppressWarnings("unused")
 public class ChatApp {
 
+    @Before(only = "home")
+    public void ensureLogin(ActionContext context) {
+        if (!context.isLoggedIn()) {
+            throw redirect("/login");
+        }
+    }
+
     @GetAction
     public void home() {
+    }
+
+    @GetAction("login")
+    public void loginForm() {
+    }
+
+    @PostAction("login")
+    public void login(String username, ActionContext context) {
+        context.login(username);
+        redirect("/");
     }
 
     @WsAction("msg")
     public void onMessage(String message, WebSocketContext context) {
         // suppress blank lines
         if (S.notBlank(message)) {
-            context.sendToPeers(message);
+            context.sendToPeers(message + " - " + context.actionContext().username());
         }
     }
 
